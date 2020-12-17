68320914394e0afc7f9e33803cf47b74dfa4dfd5
victor-ivanov-ivt20-2 <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2020-12-17 11:54:44 +0900

    пока что всё в разработке

--- js/statistics.js
+++ js/statistics.js
@@ -1,4 +1,27 @@
+
 let health = 100;
 let money = 0;
+
 document.getElementById("health").innerHTML = "Здоровье = " + health;
 document.getElementById("money").innerHTML = "Монеты = " + money;
+function hp(health) {
+  if (health > 0) {
+    health -= 10;
+    document.getElementById("health").innerHTML = "Здоровье = " + health;
+    return health;
+  }
+}
+function mon(money) {
+  if (money >= 0) {
+    money += 1;
+    document.getElementById("money").innerHTML = "Монеты = " + money;
+    return money;
+  }
+}
+function buy(money) {
+  if (money >= 0) {
+    money -= 1;
+    document.getElementById("money").innerHTML = "Монеты = " + money;
+    return money;
+  }
+}
