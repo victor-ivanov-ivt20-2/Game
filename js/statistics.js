4da0e7ee717c45df1e021da51edfb4322da8f5db
IVT202ipp <dg_gd_84@mail.ru>
2020-12-21 19:12:52 +0900

    ЛОГИКА ДРАКИ

--- js/statistics.js
+++ js/statistics.js
@@ -1,6 +1,8 @@
+let e_health = 50
 let health = 100;
 let money = 0;
-
+let heal_b = 2;
+localStorage.setItem("heal_b", heal_b);
 if (Number(localStorage.getItem("health")) > 0 &&
 Number(localStorage.getItem("money")) >= 0) {
   health = Number(localStorage.getItem("health"));
@@ -8,6 +10,7 @@ Number(localStorage.getItem("money")) >= 0) {
 }
 document.getElementById("health").innerHTML = "Здоровье = " + health;
 document.getElementById("money").innerHTML = "Монеты = " + money;
+
 function hp(health) {
   if (health > 0) {
     health -= 10;
