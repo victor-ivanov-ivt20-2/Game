cce718e3b78232712995b1b1a3ee4e45ca6352f7
victor-ivanov-ivt20-2 <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2020-12-26 08:15:51 +0900

    что-то изменил

--- static/js/start.js
+++ static/js/start.js
@@ -1,12 +1,17 @@
 if (Number(localStorage.getItem("health")) != 0) {
   document.getElementById("start__name__button").innerHTML = "продолжить";
-  document.getElementById("start__clear").innerHTML = "сброс";
+  const button__div__clear = document.querySelector(".button__div__clear");
+  let k = document.createElement("button");
+  k.setAttribute("onclick", "clear_it();");
+  k.setAttribute("id", "button__clear");
+  k.innerHTML = 'сброс';
+  button__div__clear.appendChild(k);
 }
 function if_continue() {
   if (Number(localStorage.getItem("health")) == 0) {
     localStorage.clear();
   }
-  return "удалил при начале"
+  return "удалил при начале";
 }
 function clear_it() {
   let clear_ = document.getElementById("button__clear");
